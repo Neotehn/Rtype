97b2011dee76a0bed063021836f8d3bea87e7bd3
JosefineM2000 <josefine.mende@epitech.eu>
2023-01-17 15:22:10 +0100

    [SETUP] [MODIFY] adjust to formatter

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+
 #include "SFML/Graphics.hpp"
 
 int main() {
-    std::cout << "Hello, World Client!" << std::endl;
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  std::cout << "Hello, World Client!" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        window.display();
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) window.close();
     }
-    return 0;
+    window.clear();
+    window.display();
+  }
+  return 0;
 }
