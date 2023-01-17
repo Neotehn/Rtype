60bc2d1cb0af69aa2e4eafcd271e2c724efc04fc
MarcPIster <marc.pister@epitech.eu>
2023-01-17 14:44:24 +0100

    [SETUP] [ADD] init project

---
+++ Client/clean.sh
@@ -0,0 +1,40 @@
+#Script to remove trash bevor commiting
+FILE=Makefile
+if test -f "$FILE"; then
+    rm Makefile
+fi
+
+FILE=cmake_install.cmake
+if test -f "$FILE"; then
+    rm cmake_install.cmake
+fi
+
+FILE=CmakeFiles
+if test -f "$FILE"; then
+    rm -rf CmakeFiles
+fi
+
+FILE=_deps
+if test -f "$FILE"; then
+    rm -rf _deps
+fi
+
+FILE=CmakeCache.txt
+if test -f "$FILE"; then
+    rm CmakeCache.txt
+fi
+
+FILE=CPackConfig.cmake
+if test -f "$FILE"; then
+    rm CPackConfig.cmake
+fi
+
+FILE=CPackSourceConfig.cmake
+if test -f "$FILE"; then
+    rm CPackSourceConfig.cmake
+fi
+
+FILE=r-type_server
+if test -f "$FILE"; then
+    rm r-type_client
+fi
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Rtype
+# B-CPP-500-BER-5-2-rtype-gylian.karsch
+
+# Install by running `make` in the root directory of the project
+`make clean` to clean everything\
+`make rebuild` to rebuild the project
+
