d86d6d6f1e6e2fbf95cc383dc36bd370eed67c91
MarcPIster <marc.pister@epitech.eu>
2023-01-20 09:03:41 +0100

    [CLIENT] [CLEAN] clean up code RTB-24
    Motivation:
    We want to have a clean consisting coding-style
    Modifications:
    merged with add-movement-branch
    
    Result:
    We now have clean code

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -4,17 +4,20 @@ int main() {
   Player player("./sprites/starship.png");
   sf::Texture backgroundTexture;
 
+  sf::RenderWindow window(sf::VideoMode(800, 1400),
+                          "R-Type Epitech");
+  sf::Vector2f windowSize =
+      sf::Vector2f(window.getSize().x, window.getSize().y);
+  window.setFramerateLimit(60);
+  float playerX = player.getPosition().x;
+
   if (!backgroundTexture.loadFromFile("./sprites/background2.jpg"))
     std::cerr << "Error loading background texture" << std::endl;
   sf::Sprite background(backgroundTexture);
-  sf::RenderWindow window(sf::VideoMode(800, 1400), "R-Type Epitech");
-  sf::Vector2f windowSize =sf::Vector2f (window.getSize().x, window.getSize().y);
-  window.setFramerateLimit(60);
-  float playerX = player.getPosition().x;
-  background.setPosition(sf::Vector2f(-(playerX * 0.1), 0));
+  background.setPosition(sf::Vector2f(-float(playerX * 0.1), 0));
+
   while (window.isOpen()) {
     sf::Event event;
-
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) window.close();
       if (event.type == sf::Event::KeyPressed) {
