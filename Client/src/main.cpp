158ee52ee3d053707e8f2b18a142e12f9eb0b67e
JosefineM2000 <josefine.mende@epitech.eu>
2023-01-30 11:55:33 +0100

    [SETUP] [MODIFY] adjust clang formatter

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -6,7 +6,7 @@ int main() {
 
   sf::RenderWindow window(sf::VideoMode(800, 1400), "R-Type Epitech");
   sf::Vector2f window_size =
-      sf::Vector2f(window.getSize().x, window.getSize().y);
+    sf::Vector2f(window.getSize().x, window.getSize().y);
   window.setFramerateLimit(60);
   float player_x = player.getPosition().x;
 
@@ -18,9 +18,7 @@ int main() {
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
+      if (event.type == sf::Event::Closed) { window.close(); }
       if (event.type == sf::Event::KeyPressed) {
         player.handlePlayerInput(event);
       }
