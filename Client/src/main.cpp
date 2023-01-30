465a7e31d04ac95de6f11e1e8500040e6dd311ee
JosefineM2000 <josefine.mende@epitech.eu>
2023-01-30 11:20:32 +0100

    [SETUP] [MODIFY] adjust clang formatter

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -18,7 +18,8 @@ int main() {
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) window.close();
+      if (event.type == sf::Event::Closed)
+        window.close();
       if (event.type == sf::Event::KeyPressed) {
         player.handlePlayerInput(event);
       }
