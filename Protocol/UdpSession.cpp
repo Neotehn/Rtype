09f8d0b71c9f0506746c8ff5cf8001eba386d66d
gylian <gylian.karsch@epitech.eu>
2023-01-31 11:17:30 +0100

    [SETUP] [MODIFY] Fixed Codingstyle & RTB-30

--- Protocol/UdpSession.cpp
+++ Protocol/UdpSession.cpp
@@ -1,12 +1,10 @@
 #include "./UdpSession.hpp"
 
-UdpSession::UdpSession(UdpServer *t_server) : m_server(t_server) {
-
-}
+UdpSession::UdpSession(UdpServer *t_server) : m_server(t_server) {}
 
 void UdpSession::handleSent(const error_code &t_ec, std::size_t) {
   if (t_ec) {
-    std::cout << "Error sending response to " << m_remoteEndpoint <<
-    ": " << t_ec.message() << "\n";
+    std::cout << "Error sending response to " << m_remoteEndpoint << ": "
+              << t_ec.message() << "\n";
   }
 }
