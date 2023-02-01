9584d636cb775c8128b8f7adbcb8ff6a99bdeedd
JosefineM2000 <josefine.mende@epitech.eu>
2023-02-01 11:59:10 +0100

    [CLIENT] [MODIFY] add inputs to game

--- Game/Game.hpp
+++ Game/Game.hpp
@@ -1,6 +1,9 @@
 #ifndef GAME_GAME_HPP
 #define GAME_GAME_HPP
 
+#include <memory>
+#include <vector>
+
 #include "./ECS/EntityManager.hpp"
 #include "./ECS/DataTypesECS.hpp"
 #include "./ECS/ISystem.hpp"
@@ -17,8 +20,6 @@ class Game {
   Game();
   ~Game();
 
-  sf::RenderWindow &getWindow();
-  InputManager &getInputManager();
   void run();
 
  private:
