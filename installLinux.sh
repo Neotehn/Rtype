527bc1ec262c3f00c5c9bd8e73974a03eae5e47a
MarcPIster <marc.pister@epitech.eu>
2023-02-21 13:33:14 +0100

    [SETUP] [WIP] test scripts on github4

--- installLinux.sh
+++ installLinux.sh
@@ -7,10 +7,6 @@ mkdir -p $install_folder
 sudo apt-get update
 sudo apt-get install -y libgl1-mesa-dev libglx-dev libudev-dev libopenal-dev libvorbis-dev libflac-dev libogg-dev libsfml-dev libxrandr-dev libx11-dev libxcursor-dev xvfb cmake
 
-# Move installed packages to the created folder
-sudo cp /usr/include/SFML $install_folder/
-sudo cp /usr/lib/x86_64-linux-gnu/ $install_folder/
-sudo cp /usr/share/SFML $install_folder/
 
 # Print installation summary
-echo "Packages installed to: $install_folder"
+echo "Packages installed"
